7d48c017fd51d053882d4dd6d2e3acd546dec27b
Kenny Yen <k27058083@yahoo.com.tw>
2022-06-26 21:54:52 +0900

    RL add score and rank increment

---
+++ src/app/hooks.ts
@@ -0,0 +1,14 @@
+import { useEffect, useRef } from "react";
+export const userAnimationFrame = (callback: any) => {
+  //   const requestRef = useRef();
+  //   const previousTimeRef = useRef();
+  //   const animate = (time: number = 0) => {
+  //     if (previousTimeRef.current) callback(time - previousTimeRef.current);
+  //     previousTimeRef.current = time;
+  //     requestRef.current = requestAnimationFrame(animate);
+  //   };
+  //   useEffect(() => {
+  //     requestRef.current = requestAnimationFrame(animate);
+  //     return () => cancelAnimationFrame(requestRef.current);
+  //   }, []);
+};
