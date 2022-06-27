258f2b287d973e6ed49c5b955379cca0e0006471
Kenny Yen <k27058083@yahoo.com.tw>
2022-06-27 10:49:21 +0900

    RL test requstAnimationFrame

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,14 +1,47 @@
 import { useEffect, useRef } from "react";
-export const userAnimationFrame = (callback: any) => {
-  //   const requestRef = useRef();
-  //   const previousTimeRef = useRef();
-  //   const animate = (time: number = 0) => {
-  //     if (previousTimeRef.current) callback(time - previousTimeRef.current);
-  //     previousTimeRef.current = time;
-  //     requestRef.current = requestAnimationFrame(animate);
-  //   };
-  //   useEffect(() => {
-  //     requestRef.current = requestAnimationFrame(animate);
-  //     return () => cancelAnimationFrame(requestRef.current);
-  //   }, []);
+type paramsTypes = {
+  nextAnimationFrameHandler: any;
+  // we still want to have "infinite" animations in some cases
+  duration: number;
+  shouldAnimate: boolean;
+};
+export const useAnimationFrame = ({
+  nextAnimationFrameHandler,
+  // we still want to have "infinite" animations in some cases
+  duration = Number.POSITIVE_INFINITY,
+  shouldAnimate = true,
+}: paramsTypes) => {
+  const frame = useRef(0);
+  // keep track of when animation is started
+  const firstFrameTime = useRef(performance.now());
+
+  const animate = (now: number) => {
+    // calculate at what time fraction we are currently of whole time of animation
+    let timeFraction = (now - firstFrameTime.current) / duration;
+    console.log(timeFraction);
+    if (timeFraction > 1) {
+      timeFraction = 1;
+    }
+    if (timeFraction <= 1) {
+      nextAnimationFrameHandler(timeFraction);
+      // request next frame only in cases when we not reached 100% of duration
+      if (timeFraction === 1) {
+        firstFrameTime.current = performance.now();
+        // frame.current = 0;
+      } else {
+        frame.current = requestAnimationFrame(animate);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (shouldAnimate) {
+      firstFrameTime.current = performance.now();
+      frame.current = requestAnimationFrame(animate);
+    } else {
+      cancelAnimationFrame(frame.current);
+    }
+
+    return () => cancelAnimationFrame(frame.current);
+  }, [shouldAnimate]);
 };
--- src/components/rankList/RankList.tsx
+++ src/components/rankList/RankList.tsx
@@ -1,8 +1,9 @@
 /**
  * Map.tsx - the main application file
  */
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
+import { useAnimationFrame } from "../../app/hooks";
 
 // Typing
 type StreamerType = {
@@ -10,62 +11,91 @@ type StreamerType = {
   displayName: string;
   picture: string;
   score: number;
+  newScore: number;
 };
+type StreamerArrType = StreamerType[];
 const STREAMER_DATA_URL = "https://webcdn.17app.co/campaign/pretest/data.json";
 /**
  * RankList component
  * @returns {React.ReactElement}
  */
 export default function RankList(): React.ReactElement {
-  const [streamers, setStreamers] = useState<StreamerType[]>([]);
+  const [streamers, setStreamers] = useState<StreamerArrType>([]);
+  // Get new streamre score
+  const getNewStreamerScore = useCallback((data: StreamerArrType) => {
+    return data.map((streamer: StreamerType) => {
+      // 1/5 chance to update
+      if (Math.round(Math.random() * 100) % 5 === 0) {
+        // increment within a range of 0 - 1000
+        const increaseBy = Math.round(Math.random() * 1000);
+        return { ...streamer, newScore: streamer.score + increaseBy };
+      }
+      return streamer;
+    });
+  }, []);
   useEffect(() => {
     let ignore = false;
     const fetchStreamers = async () => {
       const result = await fetch(STREAMER_DATA_URL);
       const data = await result.json();
       if (!ignore) {
-        console.log("result: ", data);
-        setStreamers(data);
+        console.log("result: ", data, getNewStreamerScore(data));
+        setStreamers(
+          getNewStreamerScore(data).sort((a, b) => b.score - a.score)
+        );
       }
     };
     fetchStreamers();
     return () => {
       ignore = true;
     };
-  }, []);
+  }, [getNewStreamerScore]);
   // Increment Streamer Score
-  const incrementStreamerScore = () => {
-    const newStreamersList = streamers.map((streamer: StreamerType) => {
-      // 1/5 chance to update
-      if (Math.round(Math.random() * 100) % 5 === 0) {
-        // increment within a range of 0 - 3000
-        const increaseBy = Math.round(Math.random() * 3000);
-        return { ...streamer, score: streamer.score + increaseBy };
-      }
-      return streamer;
-    });
-    setStreamers(newStreamersList);
+  const incrementStreamerScore = (progress: number) => {
+    if (progress < 1) {
+      setStreamers((prevState) => {
+        return prevState.map((streamer) => {
+          const newStreamer = { ...streamer };
+          if (streamer.newScore && streamer.score !== streamer.newScore) {
+            newStreamer.score = Math.ceil(streamer.newScore * (progress + 1));
+          }
+          return newStreamer;
+        });
+      });
+    } else {
+      console.log("!!!!11111111111111");
+      setStreamers((prevState) => {
+        return prevState.map((streamer) => {
+          const newStreamer = { ...streamer };
+          if (streamer.newScore && streamer.score !== streamer.newScore) {
+            newStreamer.score = streamer.newScore;
+          }
+          setStreamers(getNewStreamerScore(prevState));
+          return newStreamer;
+        });
+      });
+
+      // setStreamers((prevState) => getNewStreamerScore(prevState));
+    }
   };
   const getStreamerList = () => {
-    return [...streamers]
-      .sort((a, b) => b.score - a.score)
-      .map((streamer: StreamerType, index: number) => {
-        return (
-          <StyledStreamer key={streamer.userID}>
-            <div>{index + 1}</div>
-            <StyledAvatar imgUrl={streamer.picture} />
-            <div>{streamer.displayName}</div>
-            <StyledScore>{`${streamer.score}pt`}</StyledScore>
-          </StyledStreamer>
-        );
-      });
+    return streamers.map((streamer: StreamerType, index: number) => {
+      return (
+        <StyledStreamer key={streamer.userID}>
+          <div>{index + 1}</div>
+          <StyledAvatar imgUrl={streamer.picture} />
+          <div>{streamer.displayName}</div>
+          <StyledScore>{`${streamer.score}pt`}</StyledScore>
+        </StyledStreamer>
+      );
+    });
   };
   // Score update
-  useEffect(() => {
-    const interval = setInterval(() => {
-      incrementStreamerScore();
-    }, 1000);
-    return () => clearInterval(interval);
+  useAnimationFrame({
+    nextAnimationFrameHandler: incrementStreamerScore,
+    // shouldAnimate,
+    shouldAnimate: true,
+    duration: 1000,
   });
   return <StyledRankList>{getStreamerList()}</StyledRankList>;
 }
@@ -83,6 +113,7 @@ const StyledStreamer = styled.div`
   column-gap: 1rem;
   height: 48px;
   align-items: center;
+  transition: "all 0.3s ease 0s";
 `;
 const StyledAvatar = styled.div<{ imgUrl: string }>(
   ({ imgUrl }) => `
